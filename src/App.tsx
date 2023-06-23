aae0ef4fc1ba464982decbd7a8f0f3f706f86ca7
TheRadly <alex5011999@gmail.com>
2023-06-23 13:04:46 +0400

    Revert form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,6 @@
-import ResourceTableContainer from "./resources/ResourceTableContainer";
-
-const App = () => (
-  <ResourceTableContainer
-    pageTitle="Accounts"
-    values={[]}
-    loading={false}
-    tableFields={[]}
-    paginatorCount={1}
-    showMode
-    deleteQueryMethod={() => {}}
-    getQueryMethod={() => {}}
-    onEdit={() => {}}
-    emptyMessage="Empty array"
-    searchPlaceholder="Search placeholder"
-    dropdownPlaceholder="Dropdown placeholder"
-    createLabel="Create"
-    deleteConfirmMessage="Are you sure to want to delete this item?"
-    disabledTooltipDeleteLabel="Select items for enable delete button"
-    onCreate={() => {}}
-    confirmDeleteMessage="Are you sure to want to delete this item?"
-    onRowClick={() => {}}
-  />
-);
+const App = () => {
+  const helloWorld = "Hello world!";
+  return <div>{helloWorld}</div>;
+};
 
 export default App;
