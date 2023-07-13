ced34b87021b86b8e5262e3b6bc448a314fa6d94
TheRadly <alex5011999@gmail.com>
2023-07-13 14:24:06 +0400

    Add style for dropdown label

--- src/assets/DropdownInput/styled/dropdownInputWrapper.ts
+++ src/assets/DropdownInput/styled/dropdownInputWrapper.ts
@@ -3,6 +3,15 @@ import styled from "styled-components";
 const DropdownInputWrapper = styled.span<{ fullWidth?: boolean }>`
   width: ${(props) => (props.fullWidth ? "100%" : "50%")} !important;
 
+  & > label {
+    font-size: 12px;
+    line-height: 12px;
+    color: var(--surface-600);
+    font-weight: 400;
+    font-family: var(--font-family);
+    text-transform: capitalize;
+  }
+
   & > div {
     width: 100%;
   }
